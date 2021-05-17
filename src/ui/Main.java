0e5232ca94f84269f4e512d88903692287e30398
Елизавета Чернова <elizabethchernova@gmail.com>
2021-05-17 22:15:11 +0300

    fix

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
+        primaryStage.setTitle("2048 GAME");
+        primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
     }
 
