03d81d674b8e1773517876e6ed90f8e7078e4b2c
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2021-05-18 16:06:01 +0300

    methods for buttons

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,4 +1,26 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+    @FXML
+    private Button firstButton;
+
+    public void onClickFirst(ActionEvent actionEvent) {
+        
+    }
+
+    public void onClickSecond(ActionEvent actionEvent) {
+    }
+
+    public void onClickThree(ActionEvent actionEvent) {
+    }
+
+    public void onClickFour(ActionEvent actionEvent) {
+    }
+
+    public void onClickFive(ActionEvent actionEvent) {
+    }
 }
