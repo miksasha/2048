280bc33404b8f7ca90247fac379beb6e7898eeb0
Елизавета Чернова <elizabethchernova@gmail.com>
2021-05-18 16:59:02 +0300

    ршву

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -31,7 +31,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
         myStage.setTitle("2048 GAME");
         myStage.setScene(new Scene(root, 500, 400));
-        levels(myStage);
+        //levels(myStage);
         myStage.show();
     }
 
